fee4f1992c8302bbf86c3eed30d6c222d40abce2
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-06-07 19:14:44 +0200

    ChristmasTree commit1

---
+++ ChristmasTree.java
@@ -0,0 +1,29 @@
+package pl.coderstrust.christmas;
+
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+        printTree(-1);
+    }
+
+    public static void printTree(int treesize) {
+        if (treesize < 0) {
+            return;
+        }
+
+        for (int i = 1; i <= treesize; i++) {
+            System.out.println();
+            for (int j = treesize - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = i * 2 - 1; k > 0; k--) {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+        for (int l = (2 * treesize - 1) / 2 - 1; l > 0; l--) {
+            System.out.print(" ");
+        }
+        System.out.println("**");
+    }
+}
