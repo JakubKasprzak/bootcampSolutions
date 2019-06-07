3fb64b62c00abd5d5ba1c6c621e000ccf47a34ef
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-06-07 19:10:50 +0200

    FooBar poprawiony

--- FooBar.java
+++ FooBar.java
@@ -2,32 +2,22 @@ package pl.coderstrust.foobar;
 
 public class FooBar {
 
-//    public static void main(String[] args) {
-//        for (int i = 0; i < 101; i++) {
-//            if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println(i + " FooBar");
-//            } else if (i % 3 == 0) {
-//                System.out.println(i + " Foo");
-//            } else if (i % 5 == 0) {
-//                System.out.println(i + " Bar");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
-//
-//    }
-
     public static void main(String[] args) {
-        for (int i = 0; i < 101; i++) {
-            System.out.println();
-            System.out.print(i + " ");
+        printFooBar(100);
+    }
+
+    private static void printFooBar(int n) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            builder.append(i).append(" ");
             if (i % 3 == 0) {
-                System.out.print("Foo");
+                builder.append("Foo");
             }
             if (i % 5 == 0) {
-                System.out.print("Bar");
+                builder.append("Bar");
             }
+            System.out.println(builder.toString());
+            builder.setLength(0);
         }
-
     }
 }
