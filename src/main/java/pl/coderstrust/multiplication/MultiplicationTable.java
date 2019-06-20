0069b448dae5314e439c620ef466e9b2e364d22a
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-06-20 23:03:31 +0200

    Multiplication + test WIP

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,23 +1,36 @@
 package pl.coderstrust.multiplication;
 
+import java.util.Arrays;
+
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        printMultiplicationTable(7);
+//        getMultiplicationTable(3);
+        System.out.println(Arrays.deepToString(getMultiplicationTable(4)));
     }
 
-    public static void printMultiplicationTable(int tablesize) {
-        System.out.printf("%4s", "");
-        for (int i = 1; i <= tablesize; i++) {
-            System.out.printf("%4d", i);
-        }
-        System.out.println();
-        for (int i = 1; i <= tablesize; i++) {
-            System.out.printf("%4d", i);
-            for (int j = 1; j <= tablesize; j++) {
-                System.out.printf("%4d", i * j);
+    //    public static void getMultiplicationTable(int tablesize) {
+//        System.out.printf("%4s", "");
+//        for (int i = 1; i <= tablesize; i++) {
+//            System.out.printf("%4d", i);
+//        }
+//        System.out.println();
+//        for (int i = 1; i <= tablesize; i++) {
+//            System.out.printf("%4d", i);
+//            for (int j = 1; j <= tablesize; j++) {
+//                System.out.printf("%4d", i * j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+    public static int[][] getMultiplicationTable(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i - 1][j - 1] = i * j;
             }
-            System.out.println();
         }
+        return table;
     }
 }
