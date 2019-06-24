6756d2d7074ea4c6dc2429f69c78de997915cee2
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-06-24 13:28:04 +0200

    ChristmasTree + test WIP

--- src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
@@ -1,29 +1,45 @@
 package pl.coderstrust.christmastree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChristmasTree {
 
     public static void main(String[] args) {
-        printChristmasTree(5);
+        List<String> ChristmasTree = getChristmasTree(5);
+         ChristmasTree.forEach(System.out::println);
     }
 
-    public static void printChristmasTree(int size) {
+    public static List<String> getChristmasTree(int size) {
         if (size < 0) {
-            throw new IllegalArgumentException("treesize must be greater than zero");
+            throw new IllegalArgumentException("Number must be greater than zero");
         }
 
+        List<String> result = new ArrayList<>();
+        StringBuilder line = new StringBuilder();
         for (int i = 1; i <= size; i++) {
-            System.out.println();
+//            System.out.println();
+            line.append(" ");
             for (int j = size - i; j > 0; j--) {
-                System.out.print(" ");
+//                System.out.print(" ");
+                line.append(" ");
             }
             for (int j = i * 2 - 1; j > 0; j--) {
-                System.out.print("*");
+//                System.out.print("*");
+                line.append("*");
             }
+            result.add(line.toString());
+            line.setLength(0);
         }
-        System.out.println();
+//        System.out.println();
+        line.append(" ");
         for (int i = 0; i < size - 1; i++) {
-            System.out.print(" ");
+//            System.out.print(" ");
+            line.append(" ");
         }
-        System.out.println("**");
+//        System.out.println("**");
+        line.append("**");
+        result.add(line.toString());
+        return(result);
     }
 }
