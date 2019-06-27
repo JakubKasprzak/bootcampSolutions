4af1cb73d887a33e0bc458705dce0572844156fa
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-06-27 08:45:00 +0200

    MultiplicationTable test changed to parametrized test with three test cases

--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -1,30 +1,36 @@
 package pl.coderstrust.multiplication;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MultiplicationTableTest {
+public class MultiplicationTableTest {
 
-    @Test
-    public void shouldReturnCorrectMultiplicationTable() {
-        //given
-        int size = 4;
-        int[][] expected = {{1, 2, 3, 4}, {2, 4, 6, 8}, {3, 6, 9, 12}, {4, 8, 12, 16}};
-
-        //when
-        int[][] result = MultiplicationTable.getMultiplicationTable(size);
-
-        //then
-        assertArrayEquals(expected, result);
+    @ParameterizedTest
+    @MethodSource("MultiplicationTableArguments")
+    void shouldReturnCorrectMultiplicationTable(int size, int[][] expected) {
+        assertArrayEquals(expected, MultiplicationTable.getMultiplicationTable(size));
     }
 
-    @Test
-    public void shouldReturnIllegalArgumentException() {
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-            MultiplicationTable.getMultiplicationTable(-1);
-        });
+    private static Stream<Arguments> MultiplicationTableArguments() {
+        int[][] expected4 = {{1, 2, 3, 4}, {2, 4, 6, 8}, {3, 6, 9, 12}, {4, 8, 12, 16}};
+        int[][] expected3 = {{1, 2, 3}, {2, 4, 6}, {3, 6, 9}};
+        int[][] expected2 = {{1, 2}, {2, 4}};
+        return Stream.of(
+                Arguments.of(4, expected4),
+                Arguments.of(3, expected3),
+                Arguments.of(2, expected2));
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -5, -10, -15})
+    void shouldThrowExceptionForInvalidArgument(int number) {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> MultiplicationTable.getMultiplicationTable(number));
         assertEquals("Number must be greater than 0", thrown.getMessage());
     }
 }
