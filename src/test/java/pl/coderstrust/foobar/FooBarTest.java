d5b5c17788628c2bc6a45dc501a1af23a2b9f710
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-06-27 08:29:30 +0200

    FooBar test added more test cases

--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -12,22 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FooBarTest {
 
     @ParameterizedTest
-    @MethodSource("argumentsForShouldReturnCorrectFooBar")
-    void ShouldReturnCorrectFooBar(String[] expected, int number) {
+    @MethodSource("FooBarArguments")
+    void ShouldReturnCorrectFooBar(int number, String[] expected) {
         assertArrayEquals(expected, FooBar.getFooBar(number));
     }
 
-    private static Stream<Arguments> argumentsForShouldReturnCorrectFooBar() {
-        String[] expected = {"0 FooBar", "1 ", "2 ", "3 Foo", "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo", "10 Bar", "11 ", "12 Foo", "13 ", "14 ", "15 FooBar"};
+    private static Stream<Arguments> FooBarArguments() {
+        String[] expected15 = {"0 FooBar", "1 ", "2 ", "3 Foo", "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo", "10 Bar", "11 ", "12 Foo", "13 ", "14 ", "15 FooBar"};
+        String[] expected10 = {"0 FooBar", "1 ", "2 ", "3 Foo", "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo", "10 Bar"};
+        String[] expected5 = {"0 FooBar", "1 ", "2 ", "3 Foo", "4 ", "5 Bar"};
+        String[] expected3 = {"0 FooBar", "1 ", "2 ", "3 Foo"};
         return Stream.of(
-                Arguments.of(expected, 15));
+                Arguments.of(15, expected15),
+                Arguments.of(10, expected10),
+                Arguments.of(5, expected5),
+                Arguments.of(3, expected3));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1})
+    @ValueSource(ints = {-1, -5, -10, -15})
     void shouldThrowExceptionForInvalidArgument(int number) {
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> FooBar.getFooBar(number));
-
         assertEquals("Number cannot be lower than zero.", thrown.getMessage());
     }
 }
