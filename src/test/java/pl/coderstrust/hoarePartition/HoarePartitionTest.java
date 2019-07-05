c3e85779817f8333b2987359a0c28b4cde3b670b
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-07-05 10:42:26 +0200

    Hoare + test2

--- src/test/java/pl/coderstrust/hoarePartition/HoarePartitionTest.java
+++ src/test/java/pl/coderstrust/hoarePartition/HoarePartitionTest.java
@@ -1,10 +1,8 @@
 package pl.coderstrust.hoarePartition;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -42,8 +40,7 @@ class HoarePartitionTest {
     }
 
     private static Stream<Arguments> hoarePartitionNullArgument() {
-        int[] numbers = null;
         return Stream.of(
-                Arguments.of((Object) numbers));
+                Arguments.of((Object) null));
     }
 }
