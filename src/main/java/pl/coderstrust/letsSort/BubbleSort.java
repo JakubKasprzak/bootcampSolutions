f87b15d3fa7598e61804a4751b4d38dfa20c4459
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-07-11 15:42:37 +0200

    Lets Sort WIP

---
+++ src/main/java/pl/coderstrust/letsSort/BubbleSort.java
@@ -0,0 +1,32 @@
+package pl.coderstrust.letsSort;
+
+import java.util.Arrays;
+
+public class BubbleSort implements SortingMethod {
+        array = {64, 34, 25, 12, 22, 11, 90};
+        return sort(array);
+    }
+
+
+    public static int[] sort(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("Array cannot be null");
+        }
+
+        int[] sortedArray = array.clone();
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+            for (int j = 0; j < sortedArray.length - i - 1; j++) {
+                if (sortedArray[j] > sortedArray[j + 1]) {
+                    swap(sortedArray, j, j + 1);
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        int temp = array[b];
+        array[b] = array[a];
+        array[a] = temp;
+    }
+}
