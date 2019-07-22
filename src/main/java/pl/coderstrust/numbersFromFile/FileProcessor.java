097130cc9375d395fb7524dadbd105d892514898
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-07-22 15:13:06 +0200

    numbers from file WIP

--- src/main/java/pl/coderstrust/numbersFromFile/FileProcessor.java
+++ src/main/java/pl/coderstrust/numbersFromFile/FileProcessor.java
@@ -1,15 +1,28 @@
 package pl.coderstrust.numbersFromFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileProcessor {
+
+
     public static void main(String[] args) {
     }
 
-    public List<String> readLinesFromFile(String fileName) {
-        return null;
+    static public List<String> readLinesFromFile(String fileName) throws FileNotFoundException {
+        File input = new File("1000.txt");
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            list.add(line);
+        }
+        return list;
     }
 
-    public void writeLinesToFile(List<String> resultLines, String resultFileName) {
+    public static void writeLinesToFile(List<String> resultLines, String resultFileName) {
     }
 }
--- src/main/java/pl/coderstrust/numbersFromFile/Processor.java
+++ src/main/java/pl/coderstrust/numbersFromFile/Processor.java
@@ -1,24 +1,30 @@
 package pl.coderstrust.numbersFromFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Processor {
-    private NumbersProcessor numbersProcessor;
-    private FileProcessor fileProcessor;
+    private static NumbersProcessor numbersProcessor;
+    private static FileProcessor fileProcessor;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("1000.txt");
+        File resultFile = new File("result.txt");
+        process(inputFile.getName(), resultFile.getName());
     }
 
-    public Processor(NumbersProcessor numbersProcessor, FileProcessor fileProcessor){
+    public Processor(NumbersProcessor numbersProcessor, FileProcessor fileProcessor) {
         this.numbersProcessor = numbersProcessor;
         this.fileProcessor = fileProcessor;
     }
 
-    public void process(String fileName, String resultFileName){
+    public static void process(String fileName, String resultFileName) throws FileNotFoundException {
         List<String> linesFromFile = fileProcessor.readLinesFromFile(fileName);
+        System.out.println(linesFromFile);
         List<String> resultLines = new ArrayList<>();
-        for (String line : linesFromFile){
+        for (String line : linesFromFile) {
             resultLines.add(numbersProcessor.processLine(line));
         }
         fileProcessor.writeLinesToFile(resultLines, resultFileName);
