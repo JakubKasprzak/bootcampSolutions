106c5803e4045780ac37691cd8d62f45bb1423f3
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-07-27 16:24:20 +0200

    added polymorphism test

--- src/main/java/pl/coderstrust/figures/Square.java
+++ src/main/java/pl/coderstrust/figures/Square.java
@@ -11,7 +11,4 @@ public class Square extends Rectangle {
             this.side = side;
         }
     }
-
-    void cokolwiek() {
-    }
 }
--- src/test/java/pl/coderstrust/figures/PolymorphismTest.java
+++ src/test/java/pl/coderstrust/figures/PolymorphismTest.java
@@ -7,14 +7,52 @@ import static org.junit.Assert.assertEquals;
 class PolymorphismTest {
 
     @Test
-    void simpleTest() {
+    void polymorphismSquareTest() {
+        //given
         Square square = new Square(4);
 
-        Figure anotherSquare = new Square(4);
-        Rectangle yetAnotherSquare = square;
-        ((Square) yetAnotherSquare).cokolwiek();
+        //when
+        Figure figureSquare = new Square(4);
+        Rectangle rectangleSquare = new Square(4);
 
-        assertEquals(square.calculateArea(), anotherSquare.calculateArea(), 0.01);
-        assertEquals(square.calculateArea(), yetAnotherSquare.calculateArea(), 0.01);
+        //then
+        assertEquals(square.calculateArea(), figureSquare.calculateArea(), 0.01);
+        assertEquals(square.calculateArea(), rectangleSquare.calculateArea(), 0.01);
+    }
+
+    @Test
+    void polymorphismCircleTest() {
+        //given
+        Circle circle = new Circle(4);
+
+        //when
+        Figure figureCircle = new Circle(4);
+
+        //then
+        assertEquals(circle.calculateArea(), figureCircle.calculateArea(), 0.01);
+    }
+
+    @Test
+    void polymorphismTriangleTest() {
+        //given
+        Triangle triangle = new Triangle(4, 3);
+
+        //when
+        Figure figureTriangle = new Triangle(4, 3);
+
+        //then
+        assertEquals(triangle.calculateArea(), figureTriangle.calculateArea(), 0.01);
+    }
+
+    @Test
+    void polymorphismTrapezoidTest() {
+        //given
+        Trapezoid trapezoid = new Trapezoid(4, 3, 5);
+
+        //when
+        Figure figureTrapezoid = new Trapezoid(4, 3, 5);
+
+        //then
+        assertEquals(trapezoid.calculateArea(), figureTrapezoid.calculateArea(), 0.01);
     }
 }
