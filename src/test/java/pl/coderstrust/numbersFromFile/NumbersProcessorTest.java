9f5fbeff76424762a8058042154339a848b55d36
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-07-28 18:22:00 +0200

    NumbersProcessorTest added new argument

--- src/test/java/pl/coderstrust/numbersFromFile/NumbersProcessorTest.java
+++ src/test/java/pl/coderstrust/numbersFromFile/NumbersProcessorTest.java
@@ -21,21 +21,24 @@ class NumbersProcessorTest {
     private static Stream<Arguments> numbersProcessorArguments() {
         String line1 = "      2      3      5      7     11     13     17     19     23     29 ";
         String line2 = "";
-        String line3 = "789 sfhsiudfh  hdsau hua hhfhf 987 ";
+        String line3 = " 789 sfhsiudfh  hdsau hua hhfhf 987 ";
         String line4 = "1 1 1 1 1 1";
         String line5 = "                   ";
+        String line6 = " a sdfhdsu ahl@#$#$%sdhdsh.,  dfwrf fhv";
 
         String expected1 = "2+3+5+7+11+13+17+19+23+29=129";
         String expected2 = "empty line";
         String expected3 = "789+987=1776";
         String expected4 = "1+1+1+1+1+1=6";
         String expected5 = "empty line";
+        String expected6 = "empty line";
 
         return Stream.of(
                 Arguments.of(line1, expected1),
                 Arguments.of(line2, expected2),
                 Arguments.of(line3, expected3),
                 Arguments.of(line4, expected4),
-                Arguments.of(line5, expected5));
+                Arguments.of(line5, expected5),
+                Arguments.of(line6, expected6));
     }
 }
