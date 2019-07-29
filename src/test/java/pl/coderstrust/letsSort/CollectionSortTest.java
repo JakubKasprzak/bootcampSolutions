2f541e2484472773e6cc3e0a0ac8ea86025fcb61
A5Kc2 <51000107+A5Kc2@users.noreply.github.com>
2019-07-29 19:20:21 +0200

    Delete CollectionSortTest.java

--- src/test/java/pl/coderstrust/letsSort/CollectionSortTest.java
+++
@@ -1,7 +0,0 @@
-package pl.coderstrust.letsSort;
-
-public class CollectionSortTest extends SortingTestBase {
-    public SortingMethod getSortingMethod() {
-        return new CollectionSort();
-    }
-}
