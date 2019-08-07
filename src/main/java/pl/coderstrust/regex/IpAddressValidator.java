b1e2ea2d52865d4081f6e9c8c0f02603fcee73d2
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-08-07 15:47:16 +0200

    moved pattern to class field

--- src/main/java/pl/coderstrust/regex/IpAddressValidator.java
+++ src/main/java/pl/coderstrust/regex/IpAddressValidator.java
@@ -4,13 +4,12 @@ import java.util.regex.Pattern;
 
 public class IpAddressValidator {
     private static final String REGEX = "\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b";
+    private static final Pattern pattern = Pattern.compile(REGEX);
 
     public static boolean isIpAddress(String input) {
         if (input == null) {
             throw new IllegalArgumentException("Line cannot be null.");
         }
-        Pattern pattern = Pattern.compile(REGEX);
-//        Matcher matcher = pattern.matcher(input);
         return pattern.matcher(input).matches();
     }
 }
